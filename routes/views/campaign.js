67975b47ce5ff938e8cff341210d1748c9cdd833
Production <hector@agilekitchen.io>
2016-08-21 11:05:12 -0400

    update from production

--- routes/views/campaign.js
+++ routes/views/campaign.js
@@ -29,6 +29,42 @@ exports = module.exports = function(req, res) {
 			}
 
 			if (campaign) {
+
+				var contains = function(needle) {
+			    // Per spec, the way to identify NaN is that it is not equal to itself
+			    var findNaN = needle !== needle;
+			    var indexOf;
+
+			    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+			        indexOf = Array.prototype.indexOf;
+			    } else {
+			        indexOf = function(needle) {
+			            var i = -1, index = -1;
+
+			            for(i = 0; i < this.length; i++) {
+			                var item = this[i];
+
+			                if((findNaN && item !== item) || item === needle) {
+			                    index = i;
+			                    break;
+			                }
+			            }
+
+			            return index;
+			        };
+			    }
+
+			    return indexOf.call(this, needle) > -1;
+				};
+
+				var index = contains.call(req.user.campaigns, campaign._id);
+				console.log(index)
+				if (index) {
+					locals.participant = true;
+				} else {
+					locals.participant = false;
+				}
+
 				console.log(campaign);
         locals.campaign = campaign;
 				next();
